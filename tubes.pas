fd6eb0c209d79424c50c78011de84fc80fa1d5a3
Ahsan Athallah <athallah@users.noreply.github.com>
2017-11-08 23:27:00 +0700

    v0.01

--- tubes.pas
+++ tubes.pas
@@ -1,1 +1,25 @@
-{Tempat wisata di Emerald city dikelola datanya menggunakan sebuah program. Program tersebut mengelola data usaha wisata berdasarkan tipe wisata (outdoor, indoor), (individu, family friendly), harga tiket masuk sesuai dengan hari atau waktunya, dan menyediakan fasilitas umum seperti toilet, musholah, dan juga jenis makanan / sourvenir yang dijual ditempat pariwisata tersebut. Dan juga ada tidaknya fasilitas untuk para penyandang disabilitas.}
+{Tempat wisata di Emerald city dikelola datanya menggunakan sebuah program.
+Program tersebut mengelola data usaha wisata berdasarkan tipe wisata (outdoor, indoor), 
+(individu, family friendly), harga tiket masuk sesuai dengan hari atau waktunya, dan menyediakan 
+fasilitas umum seperti toilet, musholah, dan juga jenis makanan / sourvenir yang dijual ditempat 
+pariwisata tersebut. Dan juga ada tidaknya fasilitas untuk para penyandang disabilitas.}
+
+program JaJalKuy;
+uses crt,sysutils;
+type 
+
+//==================== PROCEDURE DAN FUNCTION ====================\\
+
+//==================== PROGRAM UTAMA ====================\\
+begin
+	writeln(']]]]]]]]]] Selamat Datang! [[[[[[[[[[');
+	repeat
+	write(' Login As : [1.User 2.Admin] : '); readln(id);
+	until ((id=1) or (id=2));
+	if id = 1 then
+		menuUser()
+	else
+		menuAdmin();
+
+end.
+
