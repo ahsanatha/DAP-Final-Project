7faf2ca12388f635b63c42c6bb6ad10bdf031a59
Ahsan Athallah <athallah@users.noreply.github.com>
2017-11-13 00:53:37 +0700

    v0.5
    added insert and view

--- tubes.pas
+++ tubes.pas
@@ -40,6 +40,11 @@ type
 			negara : string;
 			
 		end;
+	waktu =
+		record
+			Buka : Integer;
+			tutup : Integer;
+		end;
 
 	wisata = 	
 		record
@@ -47,14 +52,17 @@ type
 			inout : string;
 			indFam : string;
 			harga : hari;
-			lokasi : string;
+			lokasi : loc;
 			deskripsi : string;
-			harga : longint;
+			jam : waktu ;
+			fasilitas : array[1..100] of string;
 		end;
 var
-	id: Integer;
+	id,i,j: Integer;
 	f: file of wisata;
-	arWisata: array of [1..10000] of wisata;
+	arWisata: array [1..1000] of wisata;
+	jumlahdata:longint;
+	pilih : Byte;
 //==================== PROCEDURE DAN FUNCTION ====================\\
 procedure menuUser();
 begin
@@ -62,6 +70,52 @@ begin
 	
 end;
 //==================== Admin Menu dan teman teman ====================\\
+function getWord1() : string;
+var
+  c : char;
+
+begin
+  getWord1 := '';
+  read(c);
+  while(c <> ' ') do 
+  begin
+    getWord1 := getWord1 + c;
+    if(eoln) then 
+    begin
+      readln();
+      break;
+    end;
+    read(c);
+  end;
+end;
+function getWord() : string;
+var
+  c : char;
+begin
+  getWord := '';
+  read(c);
+  while(c <> ',') do 
+  begin
+    getWord := getWord + c;
+    if(eoln) then 
+    begin
+      readln();
+      break;
+    end;
+    read(c);
+  end;
+end;
+procedure load;
+begin
+	assign(f,'wisata.dat');
+	reset(f);
+	while not eof(f) do
+		begin
+			inc(jumlahdata);
+			read(f,arWisata[jumlahdata]);
+		end;
+	close(f);
+end;
 procedure save;
 begin
 	assign(f,'wisata.dat');
@@ -73,19 +127,90 @@ begin
 	close(f);
 end;
 procedure insertData();
+	var
+		tmp,desk: string;
+		pilihadmin:char;
 begin
 	repeat
 		clrscr;
-		write('Nama Tempat Wisata : '); readln(arWisata[i].nama);
-		write('Indoor / Outdoor : '); readln(arWisata[i].inout);
-		write('individu / Family Friedly : ') readln(arWisata[i].)
-	until pilih='n';
+		inc(jumlahdata);
+		write('Nama Tempat Wisata : '); readln(arWisata[jumlahdata].nama);
+		write('Indoor / Outdoor : '); readln(arWisata[jumlahdata].inout);
+		write('individu / Family Friendly : '); readln(arWisata[jumlahdata].indFam);
+		write('Harga Weekday [Senin-Jumat] (per orang) : '); readln(arWisata[jumlahdata].harga.weekday);
+		write('Harga Weekend [Sabtu-Minggu] (per orang) : '); readln(arWisata[jumlahdata].harga.weekend);
+		write('Jam Buka Operasional : '); readln(arWisata[jumlahdata].jam.buka);
+		write('Jam Tutup Operasional : '); readln(arWisata[jumlahdata].jam.tutup);
+		write('fasilitas [pisahkan dengan koma, akhiri dengan #] : '); j:=1; tmp:=getWord;
+		while tmp <> '#' do //masukin kata perkata sampai user menginput #
+			begin
+				arWisata[jumlahdata].fasilitas[j]:=tmp;
+				tmp:=getWord;
+				inc(j);
+			end;
+		write(' Deskripsi : '); desk:=''; tmp:=getWord1;
+		while tmp <> '#' do //masukin kata perkata sampai user menginput #
+			begin
+				desk:=desk+' '+tmp;
+				tmp:=getWord1;
+			end;
+		arWisata[i].deskripsi:=desk;
+		writeln;
+		write('Ingin nambah data wisata lagi? [Y/T]'); readln(pilihadmin);
+	until (lowercase(pilihadmin)='t');
 end;
+
+procedure viewData();
+	var
+		tmp,flag: Integer;
+begin
+	clrscr;
+	flag:=1;
+	for i:= 1 to jumlahdata do 
+		begin
+			if flag <= 2 then
+				begin
+					inc(flag);
+					writeln('#',i);
+					writeln('Nama Tempat Wisata : ', arWisata[i].nama);
+					writeln('Indoor / Outdoor : ', arWisata[i].inout);
+					writeln('individu / Family Friendly : ', arWisata[i].indFam);
+					writeln('Harga Weekday [Senin-Jumat] (per orang) : ', arWisata[i].harga.weekday);
+					writeln('Harga Weekend [Sabtu-Minggu] (per orang) : ', arWisata[i].harga.weekend);
+					writeln('Jam Operasional : ', arWisata[i].jam.buka,'-',arWisata[i].jam.tutup);
+					write('fasilitas : '); j:=1;
+						while arWisata[i].fasilitas[j] <> '' do
+						begin
+							write(arWisata[i].fasilitas[j],' ');
+							inc(j);
+						end;
+					writeln;
+					write('Deskripsi : ', arWisata[i].deskripsi);
+					writeln;
+					writeln;
+				end
+			else
+				begin
+					flag:=1;
+					write('press any key to continue.. '); readln;
+					clrscr;
+				end;
+		end;
+		writeln;
+	write('[1.Insert 2.Edit 3.Delete 4.Back] : '); readln(tmp);
+		case tmp of
+			1 : begin insertData(); end;
+			2 : begin {editData;} end;
+			3 : begin {deleteData;} end;
+		end;
+end;
+
 procedure menuAdmin();
 begin
-	buat;
+	jumlahdata:=0;
 	load;
 	repeat
+	clrscr;
 	writeln('Login >> Admin Menu');
 	writeln('== Admin Menu ==');
 	writeln('1. Insert Data');
@@ -95,28 +220,34 @@ begin
 	writeln('5. Back');
 	writeln;
 	write(' Pilih : '); readln(pilih);
-	until (pilih >= 1) and (pilih <= 5);
 	case pilih of
 		1 : begin insertData(); end;
-		2 : begin editData(); end;
-		3 : begin deleteData(); end;
+		2 : begin {editData();} end;
+		3 : begin {deleteData();} end;
 		4 : begin viewData(); end;
 	end;
-
-
+	until (pilih=5);
+	save;
 end;
 
 //==================== PROGRAM UTAMA ====================\\
 begin
 	repeat
 	clrscr;
-	writeln(']]]]]]]]]] Selamat Datang di JaJalKuy! [[[[[[[[[[');
-	write(' Login As : [1.User(not yet) 2.Admin] : '); readln(id);
-	until ((id=1) or (id=2));
-	if id = 1 then
-		menuUser()
-	else
-		menuAdmin();
-
+	writeln('-------------------------------------------------');
+	writeln('========== Selamat Datang di JaJalKuy! ==========');
+	writeln('-------------------------------------------------');
+	writeln(' Main Menu ');
+	writeln('1.User(not yet)'); 
+	writeln('2.Admin ');
+	writeln('3.Log Out');
+	writeln;
+	write('Pilih : '); readln(id);
+	case id of
+		1: menuUser();
+		2: menuAdmin();
+	end;
+	until (id=3);
+	clrscr;
 end.
 
